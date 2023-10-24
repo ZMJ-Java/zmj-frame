0df70acf1c16837230d12533b8714f1084107dfa
ZMJ1486480852 <1486480852@qq.com>
2023-10-24 15:34:15 +0800

    枚举优化

--- zmj-demo/src/main/java/com/zmj/user/designPattern/abstractFactoryPattern/demo/PayChannelEnum.java
+++ zmj-demo/src/main/java/com/zmj/user/designPattern/abstractFactoryPattern/demo/PayChannelEnum.java
@@ -1,10 +1,17 @@
 package com.zmj.user.designPattern.abstractFactoryPattern.demo;
 
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author ZMJ
  * @Package com.zmj.user.designPattern.abstractFactoryPattern.demo
  * @date 2023/10/17 19:03
  */
+@Getter
 public enum PayChannelEnum {
     WeChatPay(0, "微信支付"),
     ZhiFuBaoPay(1, "支付宝支付"),
@@ -25,12 +32,20 @@ public enum PayChannelEnum {
         this.desc = desc;
     }
 
+    public static final Map<Integer, PayChannelEnum> clientChannelMap
+            = Stream.of(PayChannelEnum.values()).collect(Collectors.toMap(e -> e.getCode(), e -> e));
+
+    public static PayChannelEnum getChannel(Integer code) {
+        PayChannelEnum payChannelEnum = clientChannelMap.get(code);
+        return payChannelEnum;
+    }
+
     /**
      * 根据code值获取枚举值
      */
     public static PayChannelEnum getPayChannelByCode(int codeVal) {
         for (PayChannelEnum payChannelEnum : PayChannelEnum.values()) {
-            if (payChannelEnum.code == codeVal){
+            if (payChannelEnum.code == codeVal) {
                 return payChannelEnum;
             }
         }
